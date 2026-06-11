cf15526445ed99683c8fc9c5d79a33dcfeb628fa
Qwekus <tema.kivel@gmail.com>
2026-06-11 18:56:24 +0300

    edited SQL (i am on 2%)

--- queries.sql
+++ queries.sql
@@ -2,47 +2,105 @@
 
 
 -- name: CreateUser :one
-INSERT INTO users (users_id, email, username, password_hash, created_at)
-VALUES(gen_random_uuid(), $1, $2, $3, NOW())
-RETURNING id;
+INSERT INTO users (email, username, password_hash, created_at)
+VALUES($1, $2, $3, NOW())
+RETURNING user_id;
 
 -- name: GetUserById :one
-SELECT id, email, username, password_hash, created_at
+SELECT user_id, email, username, password_hash, created_at
 FROM users
 WHERE user_id = $1;
 
 -- name: GetUserByEmail :one
-SELECT id, email, username, password_hash, created_at
+SELECT user_id, email, username, password_hash, created_at
 FROM users
 WHERE email = $1;
 
--- name: GetBoardTasks :many
-SELECT id
-FROM tasks 
-WHERE board_id = $1
+-- name: ChangePassword :exec
+UPDATE users 
+SET password_hash = $1
+WHERE user_id = $2;
+
+-- name: GetWorkspaceById :one
+SELECT workspace_id, title, created_at, created_by
+FROM workspaces
+WHERE workspace_id = $1;
+
+-- name: CreateWorkspace :one
+INSERT INTO workspaces (created_at, title, created_by)
+VALUES(NOW(), $1, $2)
+RETURNING workspace_id;
+
+-- name: EditWorkspace :exec
+UPDATE workspaces 
+SET title = $1
+WHERE workspace_id = $2;
+
+-- name: DeleteWorkspace :one
+DELETE FROM workspaces
+WHERE workspace_id = $1;
+
+-- name: GetUsersWorkspace :many
+SELECT workspace_id, title, created_by, created_at
+FROM workspaces
+WHERE created_by = $1
 ORDER BY created_at ASC;
 
--- name: CreateTask :one
-INSERT INTO tasks (created_at, name, description, assigned_id)
-VALUES(NOW(), $1, $2, $3)
-RETURNING id;
+-- name: AddMember :one
+INSERT INTO workspace_members (user_id, workspace_id, role)
+VALUES($1, $2, $3)
+RETURNING user_id;
 
--- name: UpdateTask :exec
-UPDATE tasks
-SET updated_at = NOW(), name = $2, description = $3, assigned_id = $4, status = $5
-WHERE id = $1;
+-- name: ManageMember :exec
+UPDATE workspace_members
+SET role = $1
+WHERE user_id = $2;
 
--- name: DeleteTask :exec
-DELETE FROM tasks
-WHERE id = $1;
+-- name: KickUser :exec
+DELETE FROM workspace_members
+WHERE user_id = $1;
+
+-- name: GetMemberRoleById :one
+SELECT role
+FROM workspace_members
+WHERE user_id = $1;
+
+-- name: CreateBoard :one
+INSERT INTO boards (title, workspace_id, created_at, created_by)
+VALUES($1, $2, NOW(), $3)
+RETURNING board_id;
 
 -- name: GetWorkspaceBoards :many
-SELECT id
+SELECT board_id
 FROM boards
 WHERE workspace_id = $1
 ORDER BY created_at ASC;
 
--- name: CreateBoard :one
-INSERT INTO boards (name, workspace_id, created_at, created_by)
-VALUES($1, $2, NOW(), $3)
-RETURNING id;
+-- name: EditBoard :exec
+UPDATE boards
+SET title = $1
+WHERE board_id = $2;
+
+-- name: DeleteBoard :exec
+DELETE FROM boards
+WHERE board_id = $1;
+
+-- name: CreateTask :one
+INSERT INTO tasks (board_id, created_by, title, description, assigned_id)
+VALUES($1, $2, $3, $4, $5)
+RETURNING task_id;
+
+-- name: ChangeTaskStatus :exec
+UPDATE tasks
+SET status = $1
+WHERE task_id = $2;
+
+-- name: DeleteTask :exec
+DELETE FROM tasks
+WHERE task_id = $1;
+
+-- name: GetTasksFromBoard :many
+SELECT task_id, board_id, created_at, created_by, updated_at, assigned_id, title, description, status
+FROM tasks 
+WHERE board_id = $1
+ORDER BY created_at ASC;
--- schema.sql
+++ schema.sql
@@ -1,50 +1,56 @@
---was written by Artyom K., don't beat my meat
+--was written by Artyom K., don't beat my meat 
 
---enum types for role and status
-CREATE TYPE member_role AS ENUM ('viewer', 'member', 'administrator');
-CREATE TYPE task_status AS ENUM ('to_do', 'in_progress', 'done');
---users
-CREATE TABLE users (
-    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    email TEXT NOT NULL UNIQUE,
-    username TEXT NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMP
-);
---workspaces (where boards are made)
-CREATE TABLE workspaces (
+--enum types for role and status 
+CREATE TYPE member_role AS ENUM ('viewer', 'member', 'administrator'); 
+CREATE TYPE task_status AS ENUM ('to_do', 'in_progress', 'done'); 
+
+--users 
+CREATE TABLE users ( 
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+    email TEXT NOT NULL UNIQUE, 
+    username TEXT NOT NULL, 
+    password_hash TEXT NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+); 
+
+--workspaces (where boards are made) 
+CREATE TABLE workspaces ( 
     workspace_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
     title TEXT, 
-    created_by UUID REFERENCES users(id) ON DELETE SET NULL, 
-    created_at TIMESTAMP
-);
---ws members (which can view/write)
-CREATE TABLE workspace_members (
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE, 
-    workspace_id UUID REFERENCES workspaces(id) ON DELETE CASCADE, 
-    role member_role
-);
---boards (which contain tasks)
-CREATE TABLE boards (
-    board_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_by UUID REFERENCES users(user_id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+); 
+
+--ws members (which can view/write) 
+CREATE TABLE workspace_members ( 
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    workspace_id UUID REFERENCES workspaces(workspace_id) ON DELETE CASCADE,
+    role member_role,
+    PRIMARY KEY (user_id, workspace_id)
+); 
+
+--boards (which contain tasks) 
+CREATE TABLE boards ( 
+    board_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
     title TEXT, 
-    workspace_id UUID REFERENCES workspaces(id) ON DELETE CASCADE, 
-    created_at TIMESTAMP, 
-    created_by UUID REFERENCES users(id) ON DELETE SET NULL
-);
---tasks
-CREATE TABLE tasks (
-    task_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    board_id UUID REFERENCES boards(id) ON DELETE CASCADE, 
-    created_by UUID REFERENCES users(id) ON DELETE SET NULL, 
-    created_at TIMESTAMP, 
-    updated_at TIMESTAMP, 
-    assigned_id UUID REFERENCES users(id) ON DELETE SET NULL, 
+    workspace_id UUID REFERENCES workspaces(workspace_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    created_by UUID REFERENCES users(user_id) ON DELETE SET NULL
+); 
+
+--tasks 
+CREATE TABLE tasks ( 
+    task_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    board_id UUID REFERENCES boards(board_id) ON DELETE CASCADE,
+    created_by UUID REFERENCES users(user_id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    assigned_id UUID REFERENCES users(user_id) ON DELETE SET NULL,
     title TEXT, 
     description TEXT, 
-    status task_status DEFAULT 'to_do'
-);
-CREATE INDEX board_of_task ON tasks(board_id);
-CREATE INDEX assigned_user ON tasks(assigned_id);
-CREATE INDEX ws_member ON workspace_members(user_id);
-CREATE UNIQUE INDEX user_email ON users(email);
+    status task_status DEFAULT 'to_do' 
+); 
+
+CREATE INDEX board_of_task ON tasks(board_id); 
+CREATE INDEX assigned_user ON tasks(assigned_id); 
+CREATE INDEX ws_member ON workspace_members(user_id); 
